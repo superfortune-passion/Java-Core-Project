38e996f63d21ff239fc1f6ac95f5c044ae988d3a
superfortune-passion <sfortunepassion0908@outlook.com>
2021-01-26 09:29:07 +0000

    feat: Radio.java implement radio

---
+++ OOPs/LessonOOP-10/src/main/java/uz/bepro/project/model/technical/Radio.java
@@ -0,0 +1,35 @@
+package uz.bepro.project.model.technical;
+
+/**
+ * @Company: {}
+ * @Author: {urunov}
+ * @Project: {Lesson-10}
+ * @Date: {2022/04/14 && 7:14 PM}
+ */
+public class Radio implements RemoteController{
+
+    @Override
+    public void changeChannel() {
+
+    }
+
+    @Override
+    public void changeValue() {
+
+    }
+
+    @Override
+    public void changeLanguage() {
+
+    }
+
+    @Override
+    public void extraMethod() {
+
+    }
+
+    @Override
+    public void defaultMethod() {
+        RemoteController.super.defaultMethod();
+    }
+}
